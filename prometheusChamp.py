0ddeb4aa975d8a890600d6058d5cdf95acc1f1d8
Michael Krausse (ehmkah) <github.20.m-krausse@spamgourmet.com>
2020-09-29 14:13:38 +0200

    configuration in proerpteis

--- prometheusChamp.py
+++ prometheusChamp.py
@@ -1,16 +1,10 @@
+import configparser
 import sys
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 def extract(value):
     return float(value[1])
 
@@ -21,18 +15,22 @@ def extract_list_of_values():
 
 
 def help():
-    print('call with prometheusChamp <USER< <PASSWORD> <QUERY>')
+    print('1.) configure something useful in application.properties \n'
+          '2.) call with prometheusChamp <QUERY> <STEP>')
 
 
 if __name__ == '__main__':
     if (len(sys.argv) == 1 or sys.argv[1] == "help"):
         help()
     else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        query = sys.argv[3]
-        r = requests.get(query,
-            auth=HTTPBasicAuth(username, password))
+        config = configparser.RawConfigParser()
+        config.read("application.properties")
+        details_config = dict(config.items("BASE"))
+        username = details_config['user']
+        password = details_config['password']
+        query = details_config['prometheus_uri'] + sys.argv[1] + "&step=" + sys.argv[2]
+        print (query)
+        r = requests.get(query, auth=HTTPBasicAuth(username, password))
         if (r.status_code == 200):
             data_for_calculation = extract_list_of_values()
             print("MAX", max(data_for_calculation))
