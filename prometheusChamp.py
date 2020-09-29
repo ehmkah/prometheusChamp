a701694c70e9de1d27b7db653de4fe6cdd5aca20
Michael Krausse (ehmkah) <github.20.m-krausse@spamgourmet.com>
2020-09-29 14:31:55 +0200

    define start and end simplier

--- prometheusChamp.py
+++ prometheusChamp.py
@@ -1,8 +1,15 @@
 import configparser
 import sys
+from datetime import datetime
 
 import requests
 from requests.auth import HTTPBasicAuth
+
+
+def help():
+    print('1.) configure something useful in application.properties \n'
+          '2.) call with prometheusChamp <QUERY> <STARTISOTIME> <ENDISOTIME> <STEPINSECONDS>\n'
+          '   example prometheusChamp <QUERY> 2020-09-22T14:20 2020-09-23:14:20 60')
 
 
 def extract(value):
@@ -14,10 +21,9 @@ def extract_list_of_values():
     return list(map(extract, values))
 
 
-def help():
-    print('1.) configure something useful in application.properties \n'
-          '2.) call with prometheusChamp <QUERY> <STEP>')
-
+def parseIsoToUnix(param):
+    result = datetime.fromisoformat(param)
+    return str(result.timestamp())
 
 if __name__ == '__main__':
     if (len(sys.argv) == 1 or sys.argv[1] == "help"):
@@ -28,9 +34,11 @@ if __name__ == '__main__':
         details_config = dict(config.items("BASE"))
         username = details_config['user']
         password = details_config['password']
-        query = details_config['prometheus_uri'] + sys.argv[1] + "&step=" + sys.argv[2]
-        print (query)
-        r = requests.get(query, auth=HTTPBasicAuth(username, password))
+        start = parseIsoToUnix(sys.argv[2])
+        end = parseIsoToUnix(sys.argv[3])
+        queryUri = details_config['prometheus_uri'] + sys.argv[1] + "&start=" + start + "&end=" + end + "&step=" + sys.argv[4]
+        print(queryUri)
+        r = requests.get(queryUri, auth=HTTPBasicAuth(username, password))
         if (r.status_code == 200):
             data_for_calculation = extract_list_of_values()
             print("MAX", max(data_for_calculation))
